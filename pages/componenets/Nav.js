47302ea4c60501919ef52b57c669e5b3a6a239f5
looph0le <mitanshpanchal@gmail.com>
2023-01-31 10:29:20 +0530

    readme

--- pages/componenets/Nav.js
+++ pages/componenets/Nav.js
@@ -1,43 +1,45 @@
+import Link from 'next/link'
+
 export default function Nav(){
   return (
 
     <div className='bg-gradient-to-b from-gray-800 to-gray-900 flex'>
 
-      <a href="/">
+      <Link href="/">
         <h1 className="p-3 text-2xl text-white font-bold">tagwalls</h1>
-      </a>
+      </Link>
 
       <div className="text-white flex my-auto text-sm">
 
-        <a href="/latest">
+        <Link href="/latest">
           <div className="p-3 border-l border-gray-500 hover:bg-gray-700">
             <h2 className="text-green-300">latest</h2>
           </div>
-        </a>
+        </Link>
 
-        <a href="/hot">
+        <Link href="/hot">
           <div className="p-3 border-l border-gray-500 hover:bg-gray-700">
             <h2 className=" text-red-500">hot</h2>
           </div>
-        </a>
+        </Link>
 
-        <a href="/toplist">
+        <Link href="/toplist">
           <div className="p-3 border-l border-gray-500 hover:bg-gray-700">
             <h2 className=" text-purple-300">toplist</h2>
           </div>
-        </a>
+        </Link>
 
-        <a href="/random">
+        <Link href="/random">
           <div className="p-3 border-l border-gray-500 hover:bg-gray-700">
             <h2 className=" text-orange-400">random</h2>
           </div>
-        </a>
+        </Link>
 
-        <a href="/tags">
+        <Link href="/tags">
           <div className="p-3 border-x border-gray-500 hover:bg-gray-700">
             <h2 className=" text-yellow-400">tags</h2>
           </div>
-        </a>
+        </Link>
 
       </div>
     </div>
